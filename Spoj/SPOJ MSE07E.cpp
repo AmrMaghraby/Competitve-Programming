d540f7479373215e370d47f59c1f485bb227c985
AmrMaghraby <amr.alkeleay@gmail.com>
2018-01-17 03:26:52 -0800

    Create SPOJ MSE07E.cpp

---
+++ Spoj/SPOJ MSE07E.cpp
@@ -0,0 +1,62 @@
+ ///IN THE NAME OF ALLAH
+ #include<bits/stdc++.h>
+ using namespace std;
+ int X[1000005],Y[1000005],Z[1000005],n;
+ long long getSum(long long m){
+   long long Sum=0;
+   for(int i=0;i<n;i++){
+     if(m>=Y[i])
+       Sum+=((Y[i]-X[i])/Z[i]+1);
+     else if(m>=X[i])
+       Sum+=((m-X[i])/Z[i]+1);
+   }
+   return Sum;
+ }
+ int main(){
+    string str="";
+  while(getline(cin,str)){
+    n=0;
+    memset(X,0,sizeof X);
+    memset(Y,0,sizeof Y);
+    memset(Z,0,sizeof Z);
+    long  long res=0;
+    istringstream iss (str);
+    int a;
+    iss >> a;
+    X[n]=a;
+    iss >> a;
+    Y[n]=a;
+    iss >> a;
+    Z[n]=a;
+    res ^= ((Y[n]-X[n])/Z[n]+1);
+    n++;
+    while(getline(cin,str)){
+        if(str=="") break;
+        istringstream iss (str);
+        int a;
+        iss >> a;
+        X[n]=a;
+        iss >> a;
+        Y[n]=a;
+        iss >> a;
+        Z[n]=a;
+        res ^= ((Y[n]-X[n])/Z[n]+1);
+        n++;
+        str="";
+    }
+    if(res==0){
+        cout<<"no corruption"<<endl;
+        continue;
+    }
+    long long L=0,H=10000000;
+    while(L<H){
+        long long m = 1LL*(H+L)/2;
+        if(getSum(m)%2==0)
+           L=m+1;
+        else
+           H=m;
+    }
+    cout<<L<<" "<<getSum(L)-getSum(L-1)<<endl;
+
+  }
+ }
