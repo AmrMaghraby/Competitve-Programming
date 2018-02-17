bb7297fbb1068a8c986b4f6553c5426f34b9e861
AmrMaghraby <amr.alkeleay@gmail.com>
2018-02-17 22:52:07 +0200

    Update UVA 501.cpp

--- UVA/UVA 501.cpp
+++ UVA/UVA 501.cpp
@@ -1,29 +1,27 @@
 #include <cstdio>
-#include <set>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int Case, M, N;
-int A[30010], U[30010];
+int A[30010];
 int main()
 {
-    freopen("input.txt","rt",stdin);
+   // freopen("input.txt","rt",stdin);
+    int Case, M, N, tmp;
     scanf("%d", &Case);
     while (Case--) {
         scanf("%d %d", &M, &N);
         for (int i = 0; i < M; ++i)
             scanf("%d", &A[i]);
-        for (int i = 0; i < N; ++i)
-            scanf("%d", &U[i]);
 
-        multiset<int> Box;
-        for (int i = 0, u = 0, a = 0; u < N; ++u, ++i)
-        {
-            while (a < U[u] && a < N)
-                Box.insert(A[a++]);
-
-            auto iter = Box.cbegin();
-            for (int j = 0; j < i; ++j)
-                ++iter;
-            printf("%d\n", *iter);
+        vector<int> Box;
+        int U, a = 0, i = 0; // a is index of A
+        while (N--) {
+            scanf("%d", &U);
+            while (a < U) { // lower_bound回傳一個iterator指向Box裡面第一個>=A[a]的元素
+                auto iter = lower_bound(Box.begin(), Box.end(), A[a]);
+                Box.insert(iter, A[a++]);
+            }
+            printf("%d\n", Box[i++]);
         }
         if (Case) printf("\n");
     }
