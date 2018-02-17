a20ed5c070dd098c39540f691de3bbf3effe2b2d
AmrMaghraby <amr.alkeleay@gmail.com>
2018-02-17 22:47:47 +0200

    Update UVA 501.cpp

--- UVA/UVA 501.cpp
+++ UVA/UVA 501.cpp
@@ -1,31 +1,31 @@
 #include <cstdio>
-#include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
- 
-int main() {
-  int T;
-  scanf("%d", &T);
-  while (T--) {
-    int M, N, num[30000];
-    scanf("%d%d", &M, &N);
-    for (int i = 0; i < M; i++) {
-      scanf("%d", &num[i]);
+int Case, M, N;
+int A[30010], U[30010];
+int main()
+{
+    freopen("input.txt","rt",stdin);
+    scanf("%d", &Case);
+    while (Case--) {
+        scanf("%d %d", &M, &N);
+        for (int i = 0; i < M; ++i)
+            scanf("%d", &A[i]);
+        for (int i = 0; i < N; ++i)
+            scanf("%d", &U[i]);
+
+        multiset<int> Box;
+        for (int i = 0, u = 0, a = 0; u < N; ++u, ++i)
+        {
+            while (a < U[u] && a < N)
+                Box.insert(A[a++]);
+
+            auto iter = Box.cbegin();
+            for (int j = 0; j < i; ++j)
+                ++iter;
+            printf("%d\n", *iter);
+        }
+        if (Case) printf("\n");
     }
-    vector<int> box;
-    int done = 0, need = 0;
-    while (N--) {
-      int index;
-      scanf("%d", &index);
-      while (box.size() < index) {
-        vector<int>::iterator it = lower_bound(box.begin(), box.end(), num[done]);
-        box.insert(it, num[done++]);
-      }
-      printf("%d\n", box[need++]);
-    }
-    if (T) {
-      puts("");
-    }
-  }
-  return 0;
+    return 0;
 }
