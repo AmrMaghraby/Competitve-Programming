a911beba204cd6e034ceffa59b09befcafb0594f
AmrMaghraby <amr.alkeleay@gmail.com>
2018-02-19 15:43:07 +0200

    Create CF476-D2-D.cpp

---
+++ Codeforces/D/CF476-D2-D.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main(void)
+{
+    int n, k ;
+
+    scanf("%d%d", &n, &k) ;
+
+    printf("%d\n", k*(6*n-1)) ;
+
+    for(int i=0;i<n;i++)
+        printf("%d %d %d %d\n", k*(6*i+1), k*(6*i+3), k*(6*i+4), k*(6*i+5)) ;
+
+    return 0 ;
+}
