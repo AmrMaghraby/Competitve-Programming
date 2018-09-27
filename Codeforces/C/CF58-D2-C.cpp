99fbc24c8a7e14e917755d50104eb92f1b2a360a
AmrMaghraby <amr.alkeleay@gmail.com>
2018-09-27 12:37:54 +0200

    Create CF58-D2-C.cpp

---
+++ Codeforces/C/CF58-D2-C.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdtr1c++.h>
+#include <ext/numeric>
+
+using namespace std;
+
+const int MAXN = 1e5 + 5;
+
+int cnt[MAXN];
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("in.in", "r", stdin);
+//  freopen("out.out", "w", stdout);
+#endif
+
+    int n, x, ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x;
+        int d = (i < n / 2 ? i : n - 1 - i);
+        x -= d;
+        if (x < 1)
+            continue;
+        cnt[x]++;
+        ans = max(ans, cnt[x]);
+    }
+    cout << n - ans << endl;
+    return 0;
+}
